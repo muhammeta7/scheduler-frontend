3d2f590c89f53ee7cbd8ab38611d3a4192da02e4
muhammeta7 <muhammeta7@gmail.com>
2020-06-25 00:17:35 -0400

    complete employee component

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClientService} from '../services/http-client.service';
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  employees: string[];
 
-  ngOnInit(): void {
+  constructor(
+      private httpClientService: HttpClientService
+  ) { }
+
+  ngOnInit() {
+    this.httpClientService.getEmployees().subscribe(
+        response => this.handleSuccessfulResponse(response),
+    );
+  }
+
+  handleSuccessfulResponse(response)
+  {
+    this.employees = response;
   }
 
 }
