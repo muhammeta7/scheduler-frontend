2fdf89327e715faf3081cb226fa53b2a13d234c5
muhammeta7 <muhammeta7@gmail.com>
2020-06-24 22:27:29 -0400

    model for user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import {HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import { LogoutComponent } from './logout/logout.component';
+import { EmploymentComponent } from './employment/employment.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
-        LogoutComponent
+        LogoutComponent,
+        EmploymentComponent
     ],
     imports: [
         BrowserModule,
