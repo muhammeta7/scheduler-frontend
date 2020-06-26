3cd815ebadc1fac54b5eca4aa7681e5a047b3b0e
muhammeta7 <muhammeta7@gmail.com>
2020-06-26 19:53:54 -0400

    add service for http-interceptor

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -15,11 +15,15 @@ export class AuthenticationService {
     constructor(private httpClient: HttpClient){}
 
     authenticate(username, password) {
+        console.log(username);
+        console.log(password);
         const headers = new HttpHeaders({ Authorization: 'Basic ' + btoa(username + ':' + password) });
         return this.httpClient.get<User>('http://localhost:8080/employees/validateLogin', {headers}).pipe(
             map(
                 userData => {
                     sessionStorage.setItem('username', username);
+                    let authString = 'Basic ' + btoa(username + ':' + password);
+                    sessionStorage.setItem('basicauth', authString);
                     return userData;
                 }
             )
