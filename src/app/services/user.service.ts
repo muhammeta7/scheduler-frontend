98fbfa814a2f79f754179f50a0ffa4f3ed581798
muhammeta7 <muhammeta7@gmail.com>
2020-06-29 15:41:08 -0400

    add user service for http requests

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../sign-up/model/user';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private CREATE_USER_URL = 'https://localhost:8080/register';
+
+  constructor(private http: HttpClient){}
+
+  createUser(user: User): Observable<User>{
+    return this.http.post<User>(this.CREATE_USER_URL, user);
+  }
+}
