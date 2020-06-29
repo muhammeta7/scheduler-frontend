3a3f7b7c5ca2312e57d11386c2d32d9c92c0a041
muhammeta7 <muhammeta7@gmail.com>
2020-06-29 15:53:25 -0400

    add http post to create user, and add methods to sign-up component

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {User} from '../sign-up/model/user';
 import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class UserService {
-  private CREATE_USER_URL = 'https://localhost:8080/register';
+    private CREATE_USER_URL = 'https://localhost:8080/register';
 
-  constructor(private http: HttpClient){}
+    constructor(private http: HttpClient) {
+    }
 
-  createUser(user: User): Observable<User>{
-    return this.http.post<User>(this.CREATE_USER_URL, user);
-  }
+    createUser(user: User): Observable<User> {
+        return this.http.post<User>(this.CREATE_USER_URL, user);
+    }
 }
